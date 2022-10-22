69df52ebb11852e05e1e2c6a126f7fecada16109
Leonardo Galante <lndgalante@gmail.com>
2022-10-22 12:43:35 -0300

    Add testnet key. Add Metaplex RPC.

--- core/urls.ts
+++ core/urls.ts
@@ -30,45 +30,59 @@ export type Provider =
   | 'ankr'
   | 'getblock'
   | 'alchemy';
-export type Network = 'mainnet' | 'devnet';
+export type Network = 'mainnet' | 'devnet' | 'testnet';
 
 export function getRpcEndpointUrl(provider: Provider, network: Network, apiKey?: string): { rpcEndpointUrl: string } {
   const endpoints = {
     localhost: {
       mainnet: 'http://localhost:8899',
       devnet: 'http://localhost:8899',
+      testnet: 'http://localhost:8899',
     },
     solana: {
       mainnet: clusterApiUrl('mainnet-beta'),
       devnet: clusterApiUrl('devnet'),
+      testnet: clusterApiUrl('testnet'),
     },
     serum: {
       mainnet: 'https://solana-api.projectserum.com',
       devnet: '',
+      testnet: '',
     },
     genesysgo: {
       mainnet: 'https://ssc-dao.genesysgo.net',
       devnet: 'https://devnet.genesysgo.net',
+      testnet: '',
     },
     allthatnode: {
       mainnet: 'https://solana-mainnet-rpc.allthatnode.com',
       devnet: 'https://solana-devnet-rpc.allthatnode.com',
+      testnet: '',
     },
     blockdaemon: {
       mainnet: 'https://try-rpc.mainnet.solana.blockdaemon.tech',
       devnet: '',
+      testnet: '',
     },
     ankr: {
       mainnet: 'https://rpc.ankr.com/solana',
       devnet: 'https://rpc.ankr.com/solana_devnet',
+      testnet: '',
     },
     getblock: {
       mainnet: 'https://sol.getblock.io/mainnet',
       devnet: '',
+      testnet: '',
+    },
+    metaplex: {
+      mainnet: 'https://api.metaplex.solana.com',
+      devnet: '',
+      testnet: '',
     },
     alchemy: {
       mainnet: `https://solana-mainnet.g.alchemy.com/v2/${apiKey}`,
       devnet: `https://solana-devnet.g.alchemy.com/v2/${apiKey}`,
+      testnet: '',
     },
   };
   const rpcEndpointUrl = endpoints[provider][network];
