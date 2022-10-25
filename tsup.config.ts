4049bbb7a30e36862210a8815fcb64d164581d0c
Leonardo Galante <lndgalante@gmail.com>
2022-10-25 08:40:56 -0300

    Improve tsup config with only one entry point

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['hooks/index.ts', 'core/index.ts', 'common/index.ts'],
+  entry: ['./index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
