fe720c6298f927695456e91b2d0294bdf6482c52
Leonardo Galante <lndgalante@gmail.com>
2022-10-27 11:35:49 -0300

    Generate dts file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./index.ts'],
-  splitting: false,
-  sourcemap: true,
+  dts: true,
   clean: true,
+  sourcemap: true,
+  splitting: false,
+  entry: ['./index.ts'],
 });
