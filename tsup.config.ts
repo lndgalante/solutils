ba106adb44b553dc93a0d78dcf50b6a662edd5ee
Leonardo Galante <lndgalante@gmail.com>
2022-11-05 03:35:48 +0000

    Avoid source maps

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   dts: true,
   clean: true,
-  sourcemap: true,
+  sourcemap: false,
   splitting: false,
   platform: 'browser',
   entry: ['./index.ts'],
