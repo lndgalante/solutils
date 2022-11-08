aeab5923d55d9a69024710c9da6e841fa37c6319
Leonardo Galante <lndgalante@gmail.com>
2022-11-08 03:48:24 +0000

    Add invariant to core methods

--- core/idl.ts
+++ core/idl.ts
@@ -1,4 +1,5 @@
 import { inflate } from 'pako';
+import invariant from 'tiny-invariant';
 import { PublicKey, Connection } from '@solana/web3.js';
 import { utf8, decodeIdlAccount, Idl, IdlInstruction } from '@project-serum/anchor/dist/browser';
 
@@ -7,15 +8,12 @@ import { getPublicKeyFromAddress } from './keypairs';
 
 export async function getIdlFromAddress(connection: Connection, address: string): Promise<{ idl: Idl }> {
   const { publicKey: programId } = getPublicKeyFromAddress(address);
-
   const [base] = await PublicKey.findProgramAddress([], programId);
 
   const idlAddress = await PublicKey.createWithSeed(base, 'anchor:idl', programId);
   const idlAccountInfo = await connection.getAccountInfo(idlAddress);
 
-  if (!idlAccountInfo) {
-    throw new Error('Unable to find IDL account');
-  }
+  invariant(idlAccountInfo, 'IDL account not found');
 
   const idlAccount = decodeIdlAccount(idlAccountInfo.data.slice(8));
   const inflatedIdl = inflate(idlAccount.data);
@@ -26,7 +24,7 @@ export async function getIdlFromAddress(connection: Connection, address: string)
 
 export function getInstructionFromIdl(idl: Idl, instructionName: string): { instruction: IdlInstruction } | Error {
   const instruction = idl.instructions.find((instruction) => instruction.name === instructionName);
-  if (!instruction) throw new Error(`Instruction ${instructionName} not found in IDL`);
+  invariant(instruction, `Instruction ${instructionName} not found in IDL`);
 
   return { instruction };
 }
