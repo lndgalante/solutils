a533751309cbd8fd2ab80a5eedca08594c4f0ec3
Leonardo Galante <lndgalante@gmail.com>
2022-11-09 11:10:31 +0100

    Remove treeshake key.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   dts: true,
   clean: true,
-  treeshake: true,
   sourcemap: false,
   platform: 'browser',
   entry: ['./index.ts'],
